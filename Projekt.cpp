edd670f0b28a7e83dd604d652a3ae153d4aabc7f
Filipzvut <246976@vutbr.cz>
2022-11-19 22:22:30 +0100

    Update Projekt.cpp

--- Projekt.cpp
+++ Projekt.cpp
@@ -3,6 +3,7 @@
 
 #include "Projekt.h"
 #include <conio.h>
+#include <windows.h>
 
 using namespace std;
 
@@ -10,10 +11,10 @@ int main()
 {
 
 	char abc[4] = "ccc";
-
-	
+	system("pause");
+	Sleep(500000);
 	int data = 5;
-	printf(" ___________\n");
+		printf(" ___________\n");
 		printf("|%s|%s|%s|\n", abc, abc, abc);
 		printf("|%s|%s|%s|\n", abc, abc, abc);
 		printf("|%s|%s|%s|\n", abc, abc, abc);
