eb670c3e143879bed36dbcb67d0840f29bc0d23b
Filipzvut <246976@vutbr.cz>
2022-11-21 17:50:54 +0100

    Update Projekt.cpp

--- Projekt.cpp
+++ Projekt.cpp
@@ -3,12 +3,14 @@
 #include <stdio.h> 
 #include <errno.h>
 #include <stdlib.h>
+#include "funkce.cpp"
+
 //https://iq.opengenus.org/ls-command-in-c/     //okomentovat kod
 //
 char test[1500];
 void _ls(const char *dir,  int op_a, int op_l)
 {
-    dir = "C:\\Users\\zadni\\Desktop\\code\\projekt";
+    dir = "C:\\Users\\zadni\\Desktop\\code\\projekt\\da";
     struct dirent *d;
     DIR *dh = opendir(dir);
     if(!dh)
@@ -26,22 +28,31 @@ void _ls(const char *dir,  int op_a, int op_l)
     {
         if(!op_a && d->d_name[0] == '.')
             continue;
-        printf("%s\n", d->d_name);
-        for(int i = 0; i <150; i++)
+        //printf("%s\n", d->d_name);
+     char *tuktuk = strtok(d->d_name, ".txt");
+        while(tuktuk != NULL)
         {
-            if (test[i] == '.')
-                i += 3;
-            test[i];
-        }   
+            printf("%s\n", tuktuk);
+
+            tuktuk = strtok(NULL, ".txt");
+        }
+        //for(int i = 0; i <150; i++)
+        //{
+        //    if (test[i] == '.')
+        //        i += 3;
+        //    test[i];
+        //}   
 
         if(op_l) printf("\n");
     }
     if(!op_l)
     printf("\n");
+
 }
-int main(int argc, char *argv[]) 
+int main(int argc, char* argv[])
 {
-    if(argc == 1)
+ 
+    if (argc == 1)
     {
         _ls(".", 0, 0);
     }
@@ -66,5 +77,5 @@ int main(int argc, char *argv[])
         }
     }
 
-  return 0;
+    return 0;
 }
