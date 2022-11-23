87a4e9443e2c31be64891781025cdbd8740b8876
Filipzvut <246976@vutbr.cz>
2022-11-23 02:24:11 +0100

    Update Projekt.cpp

--- Projekt.cpp
+++ Projekt.cpp
@@ -1,11 +1,9 @@
 ﻿// test projekt.cpp: Definuje vstupní bod pro aplikaci.
 //
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "projekt.h"
 #include <conio.h>
-#include <iostream>
+
 void add();
 void show();
 void menu();
